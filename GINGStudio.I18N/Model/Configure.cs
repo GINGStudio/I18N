55df51d1bab5a2d46b7dc12a7890f5af87aa9053
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-01-26 10:08:21 +0000

    fix json prop

--- GINGStudio.I18N/Model/Configure.cs
+++ GINGStudio.I18N/Model/Configure.cs
@@ -1,7 +1,10 @@
+using Newtonsoft.Json;
+
 namespace GINGStudio.I18N.Model
 {
     public class Configure
     {
+        [JsonProperty("fallback")]
         public string[]? Fallback { get; set; }
     }
 }
